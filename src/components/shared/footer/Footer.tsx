331ab0e624f1623a015e9761f64b1290ebb937ae
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-08-01 13:09:14 +0300

    added footer logo

--- src/components/shared/footer/Footer.tsx
+++ src/components/shared/footer/Footer.tsx
@@ -1,3 +1,17 @@
+import Image from "next/image";
+import Container from "../container/Container";
+
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="w-dvw bg-black py-8 lg:pt-[72px] lg:pb-[67px]">
+      <Container>
+        <Image
+          src="/images/footer/logo.svg"
+          alt="logo"
+          width={267}
+          height={76}
+        />
+      </Container>
+    </footer>
+  );
 }
