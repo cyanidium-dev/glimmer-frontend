e68bc75600813b7eadb1c99c14d9f0acfff21e2c
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-08-04 14:06:40 +0300

    added loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,15 @@ import { fetchSanityData } from "@/utils/fetchSanityData";
 import { allInstagramPostsQuery } from "@/lib/queries";
 import Instagram from "@/components/homePage/instagram/Instagram";
 import { Suspense } from "react";
+import Loader from "@/components/shared/loader/Loader";
 
 export default async function HomePage() {
   const allInstagramPosts = await fetchSanityData(allInstagramPostsQuery);
 
   return (
     <>
-      <Suspense>
-        <Instagram instagramPosts={allInstagramPosts.posts} />
+      <Suspense fallback={<Loader />}>
+        <Instagram instagramPosts={allInstagramPosts?.posts} />
       </Suspense>
       <MarqueeLine />
       <TelegramCTA />
