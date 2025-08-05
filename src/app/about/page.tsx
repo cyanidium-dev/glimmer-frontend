81154a46c582e1c5f3f6d78f7ca81fcf452c2cca
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-08-05 14:37:00 +0300

    added breadcrumbs

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,19 @@
+import Breadcrumbs from "@/components/shared/breadcrumbs/Breadcrumbs";
 import MarqueeLine from "@/components/shared/marquee/MarqueeLine";
 import TelegramCTA from "@/components/shared/telegramCTA/TelegramCTA";
 
 export default function AboutPage() {
+  const crumbs = [
+    { label: "Головна", href: "/" },
+    {
+      label: "Про нас",
+      href: "/about",
+    },
+  ];
+
   return (
     <>
+      <Breadcrumbs crumbs={crumbs} />
       <MarqueeLine />
       <TelegramCTA />
     </>
