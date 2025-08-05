67ba37443c4e01ec8efb3276d531b3af5034e87c
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-08-05 17:25:56 +0300

    added about info

--- src/components/aboutPage/AboutInfo.tsx
+++ src/components/aboutPage/AboutInfo.tsx
@@ -2,48 +2,80 @@ import Image from "next/image";
 
 export default function AboutInfo() {
   return (
-    <div className="flex flex-col gap-6">
-      <div className="relative w-full h-[146px] rounded-[12px] overflow-hidden">
-        <Image
-          src="/images/aboutPage/aboutInfo/imageOne.webp"
-          alt="background"
-          fill
-          className="object-cover"
-        />
-      </div>
-      <div className="flex flex-col gap-6">
-        <div className="p-3 shadow-[0_1px_3px_0_rgba(0,0,0,0.10),0_1px_2px_-1px_rgba(0,0,0,0.10)] rounded-[12px]">
-          <h3 className="mb-4 text-[14px] lg:text-[24px] font-medium leading-[120%]">
-            Ми — Glimmer
-          </h3>
-          <p className="mb-3 text-[10px] lg:text-[12px] font-light leading-[120%]">
-            Glimmer — це місце, де книги оживають і стають частиною вашої
-            історії. Ми створюємо простір для тих, хто шукає в літературі не
-            просто сюжет, а глибокі емоції, нові ідеї та натхнення.
-          </p>
-          <p className="text-[10px] lg:text-[12px] font-light leading-[120%]">
-            Ми віримо, що книга здатна змінювати світогляд і надихати на
-            найсміливіші мрії. Саме тому ми дбаємо про кожну деталь — від вибору
-            асортименту до зручності вашого замовлення.
-          </p>
+    <div className="flex flex-col gap-6 xl:gap-8">
+      <div className="flex flex-col md:flex-row-reverse gap-6 xl:gap-8">
+        <div className="relative w-full md:w-[calc(50%-12px)] xl:w-[calc(50%-16px)] h-[146px] md:min-h-full md:h-auto rounded-[12px] overflow-hidden">
+          <Image
+            src="/images/aboutPage/aboutInfo/imageOne.webp"
+            alt="background"
+            fill
+            className="object-cover"
+          />
         </div>
-        <div className="flex gap-6">
-          <div className="w-[calc(50%-12px)] p-3 shadow-[0_1px_3px_0_rgba(0,0,0,0.10),0_1px_2px_-1px_rgba(0,0,0,0.10)] rounded-[12px]">
+        <div className="flex flex-col md:flex-row gap-6 xl:gap-8 md:w-[calc(50%-12px)] xl:w-[calc(50%-16px)]">
+          <div className="md:w-[calc(50%-12px)] xl:w-[calc(50%-16px)] py-3 px-5 shadow-[0_1px_3px_0_rgba(0,0,0,0.10),0_1px_2px_-1px_rgba(0,0,0,0.10)] rounded-[12px]">
             <h3 className="mb-4 text-[14px] lg:text-[24px] font-medium leading-[120%]">
-              Як усе почалося
+              Ми — Glimmer
             </h3>
             <p className="mb-3 text-[10px] lg:text-[12px] font-light leading-[120%]">
-              Ми прагнемо зробити читання доступним, зручним і захопливим для
-              кожного. Для нас важливо не просто продавати книги, а допомагати
-              знаходити ті історії, що залишають слід у серці.
+              Glimmer — це місце, де книги оживають і стають частиною вашої
+              історії. Ми створюємо простір для тих, хто шукає в літературі не
+              просто сюжет, а глибокі емоції, нові ідеї та натхнення.
             </p>
             <p className="text-[10px] lg:text-[12px] font-light leading-[120%]">
-              Ми підтримуємо культуру читання й мріємо, щоб кожна книга ставала
-              особливим супутником на вашому життєвому шляху.
+              Ми віримо, що книга здатна змінювати світогляд і надихати на
+              найсміливіші мрії. Саме тому ми дбаємо про кожну деталь — від
+              вибору асортименту до зручності вашого замовлення.
             </p>
           </div>
-          <div className="flex flex-col gap-4 w-[calc(50%-12px)]">
-            <div className="relative w-full h-[140px] rounded-[12px] overflow-hidden">
+          <div className="flex gap-6 md:w-[calc(50%-12px)] xl:w-[calc(50%-16px)]">
+            <div className="w-[calc(50%-12px)] md:w-full py-3 pl-5 pr-1 shadow-[0_1px_3px_0_rgba(0,0,0,0.10),0_1px_2px_-1px_rgba(0,0,0,0.10)] rounded-[12px]">
+              <h3 className="mb-4 text-[14px] lg:text-[24px] font-medium leading-[120%]">
+                Як усе почалося
+              </h3>
+              <p className="pr-5 mb-3 text-[10px] lg:text-[12px] font-light leading-[110%]">
+                Ми прагнемо зробити читання доступним, зручним і захопливим для
+                кожного. Для нас важливо не просто продавати книги, а допомагати
+                знаходити ті історії, що залишають слід у серці.
+              </p>
+              <p className="pr-5 text-[10px] lg:text-[12px] font-light leading-[110%]">
+                Ми підтримуємо культуру читання й мріємо, щоб кожна книга
+                ставала особливим супутником на вашому життєвому шляху.
+              </p>
+            </div>
+            <div className="md:hidden flex flex-col gap-4 w-[calc(50%-12px)]">
+              <div className="relative w-full h-[140px] rounded-[12px] overflow-hidden">
+                <Image
+                  src="/images/aboutPage/aboutInfo/imageTwo.webp"
+                  alt="background"
+                  fill
+                  className="object-cover"
+                />
+              </div>
+              <div className="relative w-full h-[140px] rounded-[12px] overflow-hidden">
+                <Image
+                  src="/images/aboutPage/aboutInfo/imageThree.webp"
+                  alt="background"
+                  fill
+                  className="object-cover"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col md:flex-row gap-6 xl:gap-8">
+        <div className="hidden md:flex gap-4 md:w-[calc(50%-12px)] xl:w-[calc(50%-16px)]">
+          <div className="relative w-full xl:w-[calc(75%-8px)] min-h-full rounded-[12px] overflow-hidden">
+            <Image
+              src="/images/aboutPage/aboutInfo/imageFour.webp"
+              alt="background"
+              fill
+              className="object-cover object-top"
+            />
+          </div>
+          <div className="hidden xl:flex flex-col gap-4 xl:w-[calc(25%-8px)]">
+            <div className="relative w-full h-[156px] rounded-[12px] overflow-hidden">
               <Image
                 src="/images/aboutPage/aboutInfo/imageTwo.webp"
                 alt="background"
@@ -51,7 +83,7 @@ export default function AboutInfo() {
                 className="object-cover"
               />
             </div>
-            <div className="relative w-full h-[140px] rounded-[12px] overflow-hidden">
+            <div className="relative w-full h-[156px] rounded-[12px] overflow-hidden">
               <Image
                 src="/images/aboutPage/aboutInfo/imageThree.webp"
                 alt="background"
@@ -61,8 +93,8 @@ export default function AboutInfo() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col gap-6">
-          <div className="p-3 shadow-[0_1px_3px_0_rgba(0,0,0,0.10),0_1px_2px_-1px_rgba(0,0,0,0.10)] rounded-[12px]">
+        <div className="flex flex-col gap-6 xl:gap-8 md:w-[calc(50%-12px)] xl:w-[calc(50%-16px)]">
+          <div className="py-3 px-5 shadow-[0_1px_3px_0_rgba(0,0,0,0.10),0_1px_2px_-1px_rgba(0,0,0,0.10)] rounded-[12px]">
             <h3 className="mb-4 text-[14px] lg:text-[24px] font-medium leading-[120%]">
               Наша місія
             </h3>
@@ -76,15 +108,15 @@ export default function AboutInfo() {
               особливим супутником на вашому життєвому шляху.
             </p>
           </div>
-          <div className="relative w-full h-[240px] rounded-[12px] overflow-hidden">
+          <div className="md:hidden relative w-full h-[240px] sm:h-[300px] rounded-[12px] overflow-hidden">
             <Image
               src="/images/aboutPage/aboutInfo/imageFour.webp"
               alt="background"
               fill
-              className="object-cover"
+              className="object-cover object-top"
             />
           </div>
-          <div className="p-3 shadow-[0_1px_3px_0_rgba(0,0,0,0.10),0_1px_2px_-1px_rgba(0,0,0,0.10)] rounded-[12px]">
+          <div className="py-3 px-6 shadow-[0_1px_3px_0_rgba(0,0,0,0.10),0_1px_2px_-1px_rgba(0,0,0,0.10)] rounded-[12px]">
             <h3 className="mb-4 text-[14px] lg:text-[24px] font-medium leading-[120%]">
               Чому обирають нас
             </h3>
