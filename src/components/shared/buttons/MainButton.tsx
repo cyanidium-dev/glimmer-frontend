f5f4f8a3b114ce68685bd2d2d067762aa0763175
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-08-05 18:37:14 +0300

    added sending form to the telegram

--- src/components/shared/buttons/MainButton.tsx
+++ src/components/shared/buttons/MainButton.tsx
@@ -11,6 +11,7 @@ interface MainButtonProps {
   disabled?: boolean;
   isLoading?: boolean;
   onClick?: () => void | Dispatch<SetStateAction<boolean>>;
+  loadingText?: string;
 }
 
 export default function MainButton({
@@ -21,6 +22,7 @@ export default function MainButton({
   disabled = false,
   isLoading = false,
   onClick,
+  loadingText,
 }: MainButtonProps) {
   return (
     <button
@@ -31,13 +33,13 @@ export default function MainButton({
         clsx(
           `group relative overflow-hidden enabled:cursor-pointer flex items-center justify-center px-2 rounded-full ${
             variant === "primary"
-              ? "bg-main text-white"
+              ? "bg-main text-black"
               : variant === "secondary"
                 ? "bg-white text-black"
                 : "border-1 border-black text-black bg-white"
           } 
          enabled:focus-visible:brightness-125 enabled:active:brightness-125
-        disabled:opacity-30 transition duration-300 ease-in-out`,
+        disabled:opacity-60 transition duration-300 ease-in-out`,
           "w-full",
           "text-[12px] lg:text-[15px] font-medium leading-[120%]",
           className
@@ -45,10 +47,13 @@ export default function MainButton({
       )}
     >
       <div
-        className="pointer-events-none absolute inset-0 bg-[linear-gradient(110deg,_rgba(148,197,232,0.1)_0%,_rgba(148,197,232,0.7)_50%,_rgba(148,197,232,0.1)_100%)]
-bg-[length:200%_100%] opacity-0 transition-opacity duration-500 ease-in-out animate-[shimmer_2.5s_linear_infinite] xl:group-hover:opacity-100 group-active:opacity-100 group-focus-visible:opacity-100"
+        className={`pointer-events-none absolute inset-0 ${variant === "primary" ? "bg-[linear-gradient(110deg,_rgba(255,255,255,0.1)_0%,_rgba(255,255,255,0.7)_50%,_rgba(255,255,255,0.1)_100%)]" : "bg-[linear-gradient(110deg,_rgba(148,197,232,0.1)_0%,_rgba(148,197,232,0.7)_50%,_rgba(148,197,232,0.1)_100%)]"}
+bg-[length:200%_100%] opacity-0 transition-opacity duration-500 ease-in-out animate-[shimmer_2.5s_linear_infinite] ${disabled ? "" : "xl:group-hover:opacity-100 group-active:opacity-100 group-focus-visible:opacity-100"}`}
       />
-      <span className="relative z-10"> {children}</span>
+      <span className="relative z-10">
+        {" "}
+        {isLoading ? loadingText : children}
+      </span>
       {isLoading ? <LoaderIcon /> : null}
     </button>
   );
--- src/components/shared/pop-ups/NotitficationPopUp.tsx
+++ src/components/shared/pop-ups/NotitficationPopUp.tsx
@@ -20,9 +20,7 @@ export default function NotificationPopUp({
         <h3 className="max-w-[386px] mx-auto mb-4 text-[20px] xl:text-[28px] font-medium leading-[120%] text-center text-main">
           {title}
         </h3>
-        <p className="max-w-[386px] mx-auto text-[12px] font-light leading-[120%] text-center">
-          {description}
-        </p>
+        <p className="max-w-[386px] mx-auto text-center">{description}</p>
       </div>
     </Modal>
   );
