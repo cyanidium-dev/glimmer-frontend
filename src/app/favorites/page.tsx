2f8f69442f85df7ea481fcfa27406d8c9a27ef1a
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-08-08 12:07:29 +0300

    added suspense to the favorites page

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -2,6 +2,8 @@ import MarqueeLine from "@/components/shared/marquee/MarqueeLine";
 import TelegramCTA from "@/components/shared/telegramCTA/TelegramCTA";
 import Breadcrumbs from "@/components/shared/breadcrumbs/Breadcrumbs";
 import Favorites from "@/components/favoritesPage/Favorites";
+import { Suspense } from "react";
+import Loader from "@/components/shared/loader/Loader";
 
 export default function FavoritesPage() {
   const crumbs = [
@@ -15,7 +17,10 @@ export default function FavoritesPage() {
   return (
     <div className="pt-[85px]">
       <Breadcrumbs crumbs={crumbs} />
-      <Favorites />
+      <Suspense fallback={<Loader />}>
+        {" "}
+        <Favorites />
+      </Suspense>
       <MarqueeLine />
       <TelegramCTA />
     </div>
