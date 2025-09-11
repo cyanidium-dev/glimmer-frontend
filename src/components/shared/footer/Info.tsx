19e03ca819ce512334829fcea79fbcdea9704668
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-09-11 09:53:15 +0300

    updated texts

--- src/components/shared/footer/Info.tsx
+++ src/components/shared/footer/Info.tsx
@@ -13,9 +13,7 @@ export default function Info() {
         >
           0 77 837 84 53
         </a>
-        <p className="mb-8 text-white text-[14px] lg:text-[15px] font-normal">
-          Безкоштовно
-        </p>
+
         <p className="text-white text-[16px] lg:text-[18px] font-semibold">
           {WORKING_HOURS}
         </p>
