b0e850c1c20533f4702b3938251deddcdbb460f0
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-09-11 12:22:18 +0300

    Update route.ts

--- src/app/api/monopay/pubkey/route.ts
+++ src/app/api/monopay/pubkey/route.ts
@@ -1,20 +1,26 @@
 import { NextResponse } from "next/server";
-import axios from "axios";
 
 const MONOPAY_TOKEN = process.env.MONOPAY_TOKEN!;
 
 export async function GET() {
   try {
-    const response = await axios.get(
+    const response = await fetch(
       "https://api.monobank.ua/api/merchant/pubkey",
       {
         headers: {
           "X-Token": MONOPAY_TOKEN,
         },
+        cache: "no-store", // щоб завжди брати свіжі дані
       }
     );
 
-    return NextResponse.json({ pubkey: response.data.key });
+    if (!response.ok) {
+      throw new Error(`Monobank error: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+
+    return NextResponse.json({ pubkey: data.key });
   } catch (error) {
     console.error("Failed to fetch pubkey:", error);
     return new NextResponse("Error fetching pubkey", { status: 500 });
